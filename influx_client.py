f7b90d8f1b9650af3b6a33e7eca5d90c5b2f0bea
Blinking Lights <blnkn@Colussus.local>
2023-08-25 09:32:35 +0100

    Remove click entrypoint from influx_client.py

--- influx_client.py
+++ influx_client.py
@@ -1,4 +1,3 @@
-import click
 import keyring
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
@@ -66,52 +65,3 @@ def initiate_client():
     )
 
     return client
-
-
-@click.command()
-@click.option('-b', '--bucket', default="chris-bucket", help='bucket name')
-@click.option('-m', '--measurement', default="chris_measurement", help='measurement name')  # noqa E501
-@click.option('-l', '--location', default="Prague", help='location name')
-@click.option('-t', '--temperature', default=22.5, help='temperature name')
-def send_data(bucket, measurement, location, temperature):
-    influx = initiate_client()
-
-    influx.send_data(
-        bucket=bucket,
-        measurement=measurement,
-        location=location,
-        temperature=temperature
-    )
-
-    influx.client.close()
-
-    click.echo("ok")
-
-
-@click.command()
-@click.option('-b', '--bucket', default="chris-bucket", help='bucket name')
-@click.option('-m', '--measurement', default="chris_measurement", help='measurement name')  # noqa E501
-@click.option('-s', '--range_start', default="-10m", help='measurement name')
-def get_data(bucket, measurement, range_start):
-    influx = initiate_client()
-
-    data = influx.get_data(
-        bucket=bucket,
-        measurement=measurement,
-        range_start=range_start
-    )
-
-    influx.client.close()
-
-    click.echo(data)
-
-
-@click.group()
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main.add_command(get_data)
-    main.add_command(send_data)
-    main()
